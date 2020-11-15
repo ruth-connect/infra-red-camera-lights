47e3a16adf5842424d2b06aec268dac4d7be85f2
ruth <ruth@DESKTOP-HOVL7PT>
2020-11-15 16:20:27 +0000

    Simplify just to flash red light every 4 seconds.

--- src/main/java/uk/me/ruthmills/infraredcameralights/service/InfraRedCameraLightsService.java
+++ src/main/java/uk/me/ruthmills/infraredcameralights/service/InfraRedCameraLightsService.java
@@ -2,9 +2,5 @@ package uk.me.ruthmills.infraredcameralights.service;
 
 public interface InfraRedCameraLightsService {
 
-	public void on();
-
-	public void off();
-
 	public void tick();
 }
--- src/main/java/uk/me/ruthmills/infraredcameralights/service/impl/InfraRedCameraLightsServiceImpl.java
+++ src/main/java/uk/me/ruthmills/infraredcameralights/service/impl/InfraRedCameraLightsServiceImpl.java
@@ -1,22 +1,12 @@
 package uk.me.ruthmills.infraredcameralights.service.impl;
 
-import static com.pi4j.io.gpio.PinState.HIGH;
-import static com.pi4j.io.gpio.RaspiPin.GPIO_08;
-import static com.pi4j.io.gpio.RaspiPin.GPIO_09;
+import static com.pi4j.io.gpio.PinState.LOW;
+import static com.pi4j.io.gpio.RaspiPin.GPIO_00;
 
 import javax.annotation.PostConstruct;
 
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
@@ -31,57 +21,23 @@ public class InfraRedCameraLightsServiceImpl implements InfraRedCameraLightsServ
 	private String endpoint;
 
 	private volatile GpioController gpio;
-	private volatile GpioPinDigitalOutput infraRedLeds;
 	private volatile GpioPinDigitalOutput redLed;
-
-	private RestTemplate restTemplate;
-	private final Logger logger = LoggerFactory.getLogger(InfraRedCameraLightsServiceImpl.class);
 
 	@PostConstruct
 	public void initialise() {
 		gpio = GpioFactory.getInstance();
 
-		infraRedLeds = gpio.provisionDigitalOutputPin(GPIO_08, "Infra-Red LEDs", HIGH);
-		infraRedLeds.setShutdownOptions(true, HIGH);
-
-		redLed = gpio.provisionDigitalOutputPin(GPIO_09, "Red LED", HIGH);
-		redLed.setShutdownOptions(true, HIGH);
-
-		restTemplate = new RestTemplate(getClientHttpRequestFactory());
-
-		logger.info("About to send POST to " + endpoint);
-		try {
-			restTemplate.postForEntity(endpoint, new HttpEntity<String>(""), String.class);
-		} catch (Exception ex) {
-			logger.error("Failed sending initialise request", ex);
-		}
-	}
-
-	private ClientHttpRequestFactory getClientHttpRequestFactory() {
-		int timeout = 9000;
-		RequestConfig config = RequestConfig.custom().setConnectTimeout(timeout).setConnectionRequestTimeout(timeout)
-				.setSocketTimeout(timeout).build();
-		CloseableHttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
-		return new HttpComponentsClientHttpRequestFactory(client);
-	}
-
-	@Override
-	public void on() {
-		infraRedLeds.low();
-	}
-
-	@Override
-	public void off() {
-		infraRedLeds.high();
+		redLed = gpio.provisionDigitalOutputPin(GPIO_00, "Red LED", LOW);
+		redLed.setShutdownOptions(true, LOW);
 	}
 
 	@Override
 	public void tick() {
-		redLed.low();
+		redLed.high();
 		try {
 			Thread.sleep(250);
 		} catch (InterruptedException ex) {
 		}
-		redLed.high();
+		redLed.low();
 	}
 }
